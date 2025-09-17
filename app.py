1fa079c2f1b5e2397602a52462f5afdc720a5c15
PieroJesus <piero.sotelo@vallegrande.edu.pe>
2025-09-17 00:58:32 -0500

    Primera versión de Florería Manu con sitemap

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_from_directory
 
 app = Flask(__name__)
 
+# Ruta para el sitemap
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('static', 'manu.xml', mimetype='application/xml')
+
+# Página principal
 @app.route('/')
-def index():
+def home():
     # Llamamos cada imagen por separado
-    imagen1 = url_for('static', filename='image/ramo1.png')
-    imagen2 = url_for('static', filename='image/ramo2.png')
-    imagen3 = url_for('static', filename='image/ramo3.png')
-    imagen4 = url_for('static', filename='image/ramo4.png')
-    carrito = url_for('static', filename='image/carrito.png')
-    favorito = url_for('static', filename='image/favorito.png')
-    perfil = url_for('static', filename='image/perfil.png')
-    descuento1 = url_for('static', filename='image/descuento1.png')
-    descuento2 = url_for('static', filename='image/descuento2.png')
-    descuento3 = url_for('static', filename='image/descuento3.png')
-    descuento4 = url_for('static', filename='image/descuento4.png')
-    descuento5 = url_for('static', filename='image/descuento5.png')
-    buscador = url_for('static', filename='image/buscador.png')
+    imagenes = {
+        "imagen1": url_for('static', filename='image/ramo1.jpg'),
+        "imagen2": url_for('static', filename='image/ramo2.jpg'),
+        "imagen3": url_for('static', filename='image/ramo3.png'),
+        "imagen4": url_for('static', filename='image/ramo4.png'),
+        "carrito": url_for('static', filename='image/carrito.png'),
+        "favorito": url_for('static', filename='image/favorito.png'),
+        "perfil": url_for('static', filename='image/perfil.png'),
+        "descuento1": url_for('static', filename='image/descuento1.png'),
+        "descuento2": url_for('static', filename='image/descuento2.png'),
+        "descuento3": url_for('static', filename='image/descuento3.png'),
+        "descuento4": url_for('static', filename='image/descuento4.png'),
+        "descuento5": url_for('static', filename='image/descuento5.png'),
+        "buscador": url_for('static', filename='image/buscador.png')
+    }
 
+    return render_template('index.html', **imagenes)
 
-    return render_template('index.html')
-
+# Página de formulario
 @app.route('/formulario')
 def formulario():
     return render_template('formulario.html')
